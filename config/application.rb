de6a2c11bf79d34aecf319b5ef9d2ee18d1803c2
Anuvis <ameji012@gmail.com>
2016-12-12 20:50:21 -0500

    edits to app

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+if !Rails.env.production?
+    Bundler.require(*Rails.groups)
+    Dotenv::Railtie.load
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
